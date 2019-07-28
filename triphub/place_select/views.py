8938facf68b4a2cd4da1c09f99865d34adfa1b73
Tedhoon <gt0305@likelion.org>
2019-07-29 01:40:29 +0900

    ggg

--- triphub/place_select/views.py
+++ triphub/place_select/views.py
@@ -10,7 +10,8 @@ def place_select(request):
 
 def attraction(request):
     attraction_get = []
-    introduction_get = []
+    
+    total = {}
     if request.method == 'GET':
         pick = request.GET['select_place']
         
@@ -42,22 +43,15 @@ def attraction(request):
                         attractions = soup2.findAll('div' , {'class' : 'info'})
                         
                         for attraction in attractions:
-                            at = attraction.find_all('strong' ,{'class' : 'tit'})
+                            attraction_name = attraction.find_all('strong' ,{'class' : 'tit'})
                             introduction= attraction.find_all('span' ,{'class' : 'txt'})
                             
-                            
-                            
-                            for hi in at:
-                                hello = hi.text
-                                attraction_get.append(hello)
+                            for i,v in zip(attraction_name,introduction):
+                                total[i.text]=v.text
 
-                            for hi2 in introduction:
-                                hello2 = hi2.text
-                                introduction_get.append(hello2)
-                
                         
 
-    return render(request , 'place_select.html' , {'attraction' : attraction_get , 'introduction':introduction_get, 'value' : pick})
+    return render(request , 'place_select.html' , {'attraction' : total, 'value' : pick})
 
 
 
@@ -139,6 +133,3 @@ def attraction2(request):
 
                                 
     return render(request , 'next_select/' + area + '.html' , {'attraction' : attraction_get , 'value' : pick})
-
-                                
-
