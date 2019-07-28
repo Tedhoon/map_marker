dc1d8e483f2d41ba7412c866f0fa7cf30d23027b
Tedhoon <gt0305@likelion.org>
2019-07-28 17:48:41 +0900

    for_aws

--- triphub/triphub/settings.py
+++ triphub/triphub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@4yiaxy@tm-ap^g@(^bx@#5khti0i3_%#5yectz0^4abu7f$2+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
